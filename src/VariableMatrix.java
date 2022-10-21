98c77b0b62f6f3f699646b1b7486523d20361a95
pi8901 <pi8901@gmx.de>
2022-10-21 07:46:54 +0200

    simplify

--- src/VariableMatrix.java
+++ src/VariableMatrix.java
@@ -1,63 +1,35 @@
-
 public class VariableMatrix 
 {
 	public static int xStart = 1;
 	public static int xEnde = 5;
 	public static int yStart = 10;
 	public static int yEnde = 15;
-	public static int modus = 4;
+	public static int modus = 1;
 
 	public static void main(String[] args) 
 	{
-		if(modus == 1)
+		for(int i = yStart; i <= yEnde; i++)
 		{
-			
-			for(int i = yStart; i <= yEnde; i++)
+			for(int j = xStart; j <= xEnde; j++)
 			{
-				for(int j = xStart; j <= xEnde; j++)
+				if(modus == 1)
 				{
 					System.out.print( "" + i +"+" + j + "=" + j + i + "\t");
 				}
-				System.out.println("");
-			}
-		}
-			
-		if(modus == 2) 
-		{
-			for(int i = yStart; i <= yEnde; i++)
-			{
-				for(int j = xStart; j <= xEnde; j++)
+				if(modus == 2)
 				{
 					System.out.print( "" + i + "-" + j + "=" + (j - i) + "\t");
 				}
-				System.out.println("");
-			}
-		}
-			
-		if(modus == 3)
-		{
-			for(int i = yStart; i <= yEnde; i++)
-			{
-				for(int j = xStart; j <= xEnde; j++)
+				if(modus == 3)
 				{
 					System.out.print( "" + i +"*" + j + "=" + (float) j * i + "\t");
 				}
-				System.out.println("");
-			}
-		}
-			
-		if(modus == 4)
-		{
-			for(int i = yStart; i <= yEnde; i++)
-			{
-				for(int j = xStart; j <= xEnde; j++)
+				if(modus == 4)
 				{
 					System.out.print( "" + i +"/" + j + "=" + (float) i / j + "\t");
-				}
-				System.out.println("");
+				}			
 			}
+			System.out.println("");
 		}
 	}
-
 }
-
